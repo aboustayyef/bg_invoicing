296c43dfed075e5989617e0a140e2be2d37707cd
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-06-21 07:10:07 +0000

    added removing items from invoice functionality. still need to implement from UI

--- src/js/modules/invoicing.js
+++ src/js/modules/invoicing.js
@@ -2,22 +2,13 @@ var $ = require('jquery');
 
 var init = function(){
 
-// Dummy invoice
-	bg.invoice = {
-		count:2,
-		items:[
-			{
-				code:'BR-BED-MOLTO-0001',
-				count:2,
-				total_price:0
-			},{
-				code:'BR-POOF-S-0005',
-				count:1,
-				total_price:0
-			}
-		],
+bg.invoice = {
+	count:0,
+	items:[],
 	};
+/*
 
+ */
 	bg.goodsLookupById = function(code){
 		var result = false;
 		$.each(bg.goods, function(i, item){
@@ -30,17 +21,21 @@ var init = function(){
 
 	// renders invoice based on bg.invoice
 	bg.updateInvoice=function(){
+
+		var html='';
+
 		if (bg.invoice.count > 0) {
-			var html='';
 			$.each(bg.invoice.items, function(i, item){
 				var stock_item = bg.goodsLookupById(item.code)
 				html += '<h3>' + stock_item.Code + ': ' + item.count + ' </h3>';
 				html += '<p>' + stock_item.Description + '</p>';
 			});
-			$('#output').html(html);
 		} else {
-
+			html = "<h4>No Items in Invoice Yet</h4>";
 		}
+
+		$('#output').html(html);
+
 	};
 
 	// add item with id to bg.invoice
@@ -50,10 +45,27 @@ var init = function(){
 		var stock_item = bg.goodsLookupById(code);
 
 		if (stock_item) {
+			bg.invoice.count += 1;
 			bg.invoice.items.push({code:code,count:1,total_price:0});
 			bg.updateInvoice();
 		}
 
+	};
+
+	// remove item from bg.invoice
+	bg.removeItemFromInvoice = function(code){
+
+		var new_invoice = {count:0,items:[]};
+		
+		$.each(bg.invoice.items, function(i, item){
+			if (item.code != code) {
+				new_invoice.count += 1;
+				new_invoice.items.push(item);
+			}
+		});
+		
+		bg.invoice = new_invoice;
+		bg.updateInvoice();
 	};
 
 	// Listening to add button from search result list
